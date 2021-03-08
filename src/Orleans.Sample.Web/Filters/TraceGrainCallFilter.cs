912abd1afcb48c9b7d292f0090a78c4d5735be25
Shaun Tabone <shaun.tabone@gig.com>
2021-03-08 19:07:04 +0100

    Added Logging in Filter for Debugging

--- src/Orleans.Sample.Web/Filters/TraceGrainCallFilter.cs
+++ src/Orleans.Sample.Web/Filters/TraceGrainCallFilter.cs
@@ -1,5 +1,6 @@
 using Dynatrace.OneAgent.Sdk.Api;
 using Dynatrace.OneAgent.Sdk.Api.Enums;
+using Microsoft.Extensions.Logging;
 using Orleans.Runtime;
 using System.Threading.Tasks;
 
@@ -7,39 +8,55 @@ namespace Orleans.Sample.Web.Filters
 {
     public class TraceGrainCallFilter : IOutgoingGrainCallFilter, IIncomingGrainCallFilter
     {
+        private readonly ILogger<TraceGrainCallFilter> _logger;
         private readonly IOneAgentSdk _oneAgentSdk;
 
-        public TraceGrainCallFilter()
+        public TraceGrainCallFilter(ILogger<TraceGrainCallFilter> logger)
         {
+            _logger = logger;
             _oneAgentSdk = OneAgentSdkFactory.CreateInstance();
         }
-        
+
         public async Task Invoke(IOutgoingGrainCallContext context)
         {
             var tracer = _oneAgentSdk.TraceOutgoingMessage(
-                _oneAgentSdk.CreateMessagingSystemInfo("Orleans", context?.Grain?.ToString(), MessageDestinationType.QUEUE, ChannelType.IN_PROCESS,
+                _oneAgentSdk.CreateMessagingSystemInfo("Orleans", context?.Grain?.ToString(),
+                    MessageDestinationType.QUEUE, ChannelType.IN_PROCESS,
                     ""));
 
             if (RequestContext.Get("TraceId") != null)
             {
                 tracer.SetCorrelationId(RequestContext.Get("TraceId").ToString());
-            }
 
-            await tracer.TraceAsync(async () => { await context.Invoke(); });
+                _logger.LogDebug("Tracing Outgoing {grainType} Correlation Id {traceId}", context.Grain.GetType().FullName, RequestContext.Get("TraceId"));
+
+                await tracer.TraceAsync(async () => { await context.Invoke(); });
+            }
+            else
+            {
+                await context.Invoke();
+            }
         }
 
         public async Task Invoke(IIncomingGrainCallContext context)
         {
             var tracer = _oneAgentSdk.TraceIncomingMessageProcess(
-                _oneAgentSdk.CreateMessagingSystemInfo("Orleans", context?.Grain?.ToString(), MessageDestinationType.QUEUE, ChannelType.IN_PROCESS,
+                _oneAgentSdk.CreateMessagingSystemInfo("Orleans", context?.Grain?.ToString(),
+                    MessageDestinationType.QUEUE, ChannelType.IN_PROCESS,
                     ""));
 
             if (RequestContext.Get("TraceId") != null)
             {
                 tracer.SetCorrelationId(RequestContext.Get("TraceId").ToString());
-            }
+                
+                _logger.LogDebug("Tracing Incoming {grainType} Correlation Id {traceId}", context.Grain.GetType().FullName, RequestContext.Get("TraceId"));
 
-            await tracer.TraceAsync(async () => { await context.Invoke(); });
+                await tracer.TraceAsync(async () => { await context.Invoke(); });
+            }
+            else
+            {
+                await context.Invoke();
+            }
         }
     }
 }
